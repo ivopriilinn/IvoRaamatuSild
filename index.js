8a62a4ee4f348f28c1d3c4b22e48cc74801a617e
Ann-JK <annabelkongas@gmail.com>
2023-12-07 16:44:50 +0200

    get book endpoint by id

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ const port = 8080
 const swaggerUi = require('swagger-ui-express')
 const swaggerDocument = require('./docs/swagger.json')
 
+const books = [
+    "Rikkaks saamise õpik",
+    "Pride and Prejudice",
+    "The Bear and The Nightingale",
+    "1984",
+    "Mockingjay",
+    "Capitalist Realism",
+    "Nine Perfect Strangers",
+    "Rehepapp",
+    "Kevade"
+]
 app.get('/books', (req, res) => {
-    res.send(["Jane Eyre", "The Bear and The Nightingale", "Good Omens"])
+    res.send(books)
+})
+
+app.get('/books/:id', (req, res) => {
+    res.send(books[req.params.id])
 })
 
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
