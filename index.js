d2722782e34cd694b4b6f1554f4cd53173ff55c3
Ann-JK <annabelkongas@gmail.com>
2024-01-11 15:05:58 +0200

    As a user I can see a list of books [Delivers #186825609]

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 8080
 const swaggerUi = require('swagger-ui-express')
 const yamljs = require('yamljs')
 const swaggerDocument = yamljs.load('./docs/swagger.yaml')
 
+app.use(cors())
 app.use(express.json())
 
 const books = [
     {id: 1, title:"Rikkaks saamise õpik. Kolmas täiendatud trükk", author: "Jaak Roosaare", year: 2018, pages: 416},
     {id: 2, title:"Pride and Prejudice", author: "Jane Austen", year: 2008, pages: 480},
-    {id: 3, title:"The Bear and The Nightingale", author: "Katherine Arden", year: 2017, pages: 3014}
+    {id: 3, title:"The Bear and The Nightingale", author: "Katherine Arden", year: 2017, pages: 3014},
+    {id: 4, title:"The Girl in the Tower", author: "Katherine Arden", year: 2018, pages: 360}
 ]
 app.get('/books', (req, res) => {
     res.send(books)
